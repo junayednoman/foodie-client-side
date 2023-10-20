3e3122a312015387889564a9b4863330e6258c77
junayednoman <junayednoman05@gmail.com>
2023-10-20 21:54:53 +0600

    product details page done

--- src/pages/add product/AddProduct.jsx
+++ src/pages/add product/AddProduct.jsx
@@ -25,7 +25,6 @@ const AddProduct = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 toast('Product has been added successfully!')
             })
     }
